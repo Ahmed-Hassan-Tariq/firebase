6b9fad55bc2389bbae4208ebf283918ea0cd5f80
Ahmed Hassan <atariq344@gmail.com>
2022-04-24 04:28:18 +0500

    commision

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,14 @@
+
+const firebaseConfig = {
+    apiKey: "AIzaSyB6PH7ywnnVer88772oTzWJnxICaLNtW9k",
+    authDomain: "meeme-2c986.firebaseapp.com",
+    projectId: "meeme-2c986",
+    storageBucket: "meeme-2c986.appspot.com",
+    messagingSenderId: "1071619500164",
+    appId: "1:1071619500164:web:1daac89f1e03d4b33efad1",
+    measurementId: "G-Z4EWCZN0WT"
+  
+  
+  }
+
+  export default firebaseConfig;
